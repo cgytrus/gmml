4066d4686a613db2f44df7317e3935685701a1a4
ConfiG <cgytrus@cgyt.ru>
2022-03-31 23:15:19 +0300

    simple hooks

---
+++ GmmlHooker/src/Hooker.cs
@@ -0,0 +1,32 @@
+﻿using GmmlPatcher;
+
+using UndertaleModLib;
+using UndertaleModLib.Models;
+
+namespace GmmlHooker;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Hooker : IGameMakerMod {
+    public void Load(int audioGroup, UndertaleData data, IReadOnlyList<ModMetadata> availableDependencies,
+        IEnumerable<ModMetadata> queuedMods) { /* TODO: define hooks in JSON? maybe? */ }
+
+    public static void SimpleHook(UndertaleData data, string code, string hook) {
+        UndertaleCode hookedCode = data.Code.ByName(code);
+
+        UndertaleCode originalCodeCopy = new() {
+            Name = data.Strings.MakeString($"{hookedCode.Name.Content}_orig"),
+            LocalsCount = hookedCode.LocalsCount,
+            ArgumentsCount = hookedCode.ArgumentsCount,
+            WeirdLocalsFlag = hookedCode.WeirdLocalsFlag,
+            WeirdLocalFlag = hookedCode.WeirdLocalFlag
+        };
+        originalCodeCopy.Replace(hookedCode.Instructions);
+        data.Code.Add(originalCodeCopy);
+        data.Scripts.Add(new UndertaleScript {
+            Name = originalCodeCopy.Name,
+            Code = originalCodeCopy
+        });
+
+        hookedCode.ReplaceGML(hook, data);
+    }
+}
--- GmmlSampleMod/src/SampleMod.cs
+++ GmmlSampleMod/src/SampleMod.cs
@@ -1,18 +1,20 @@
-﻿using GmmlPatcher;
+﻿using GmmlHooker;
+
+using GmmlPatcher;
 
 using UndertaleModLib;
 
 namespace GmmlSampleMod;
 
 // ReSharper disable once UnusedType.Global
-public class GameMakerMod : IGameMakerMod {
+public class SampleMod : IGameMakerMod {
     public void Load(int audioGroup, UndertaleData data, IReadOnlyList<ModMetadata> availableDependencies,
         IEnumerable<ModMetadata> queuedMods) {
         if(audioGroup != -1) return;
         try {
             // works only in Will You Snail
-            data.Code.First(code => code.Name.Content == "gml_Object_obj_epilepsy_warning_Create_0")
-                .AppendGML("txt_1 = \"eat my nuts\"", data);
+            Hooker.SimpleHook(data, "gml_Object_obj_epilepsy_warning_Create_0",
+                "gml_Object_obj_epilepsy_warning_Create_0_orig()\ntxt_1 = \"eat my nuts\"\ntxt_2=\"making sure it actually works\"");
         }
         // UndertaleModLib is trying to write profile cache but fails, we don't care
         catch(Exception) { /* ignored */ }
