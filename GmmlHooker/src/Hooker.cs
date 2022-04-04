a242c2b583aa5d887505ec0fcd299e6da0cf046e
ConfiG <cgytrus@cgyt.ru>
2022-04-04 18:26:48 +0300

    gmml_read_all_text

--- GmmlHooker/src/Hooker.cs
+++ GmmlHooker/src/Hooker.cs
@@ -17,7 +17,15 @@ public class Hooker : IGameMakerMod {
 
     public void Load(int audioGroup, UndertaleData data, ModMetadata currentMod,
         IReadOnlyList<ModMetadata> availableDependencies, IEnumerable<ModMetadata> queuedMods) {
+        if(audioGroup != -1) return;
         // TODO: define hooks in JSON? maybe?
+
+        CreateSimpleScript(data, "gmml_read_all_text", @"
+var file_buffer = buffer_load(argument0);
+var text = buffer_read(file_buffer, buffer_string);
+buffer_delete(file_buffer);
+return text
+", 1);
     }
 
     public static void ReplaceGmlSafe(UndertaleCode code, string gmlCode, UndertaleData data) {
