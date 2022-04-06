40f024da847084f3b3ae5d42b64ce35ebd9ce798
ConfiG <cgytrus@cgyt.ru>
2022-04-06 14:19:49 +0300

    fix example config

--- GmmlExampleMod/src/ExampleMod.cs
+++ GmmlExampleMod/src/ExampleMod.cs
@@ -1,19 +1,20 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 using GmmlHooker;
 
 using GmmlPatcher;
 
+using UndertaleModLib.Models;
+
 namespace GmmlExampleMod;
 
 // see https://github.com/cgytrus/WysApi/WysExampleMod for more examples
-// ReSharper disable once ClassNeverInstantiated.Global
+// ReSharper disable once UnusedType.Global
 public class ExampleMod : IGameMakerMod {
-    private struct Config {
-        public string logText { get; }
-
-        [JsonConstructor]
-        public Config(string logText) => this.logText = logText;
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Local")]
+    private class Config {
+        public string logText { get; private set; } = "";
     }
 
     public void Load(int audioGroup, ModMetadata currentMod, IEnumerable<ModMetadata> dependencies) {
@@ -25,7 +26,7 @@ if argument1 == false {{
     return false
 }}
 show_debug_message(argument0)
-show_debug_message({config.logText})
+show_debug_message({new UndertaleString(config.logText)})
 return true", 2);
     }
 }
